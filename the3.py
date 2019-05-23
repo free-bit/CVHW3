0e2a3af3db8d6dd557431e605f1941afa6aa5a2f
free-bit <erkin.turkoz@metu.edu.tr>
2019-05-23 21:20:20 +0300

    Done:
    * Refactoring
    
    * CNN layers

--- the3.py
+++ the3.py
@@ -1,100 +1,151 @@
-# Feel free to change / extend / adapt this source code as needed to complete the homework, based on its requirements.
-# This code is given as a starting point.
-#
-# REFEFERENCES
-# The code is partly adapted from pytorch tutorials, including https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+#!/usr/bin/env python3
 
 # ---- hyper-parameters ----
 # You should tune these hyper-parameters using:
 # (i) your reasoning and observations, 
 # (ii) by tuning it on the validation set, using the techniques discussed in class.
 # You definitely can add more hyper-parameters here.
-batch_size = 16
-max_num_epoch = 100
-hps = {'lr':0.001}
+# batch_size = 16
+# max_num_epoch = 100
+# hps = {'lr':0.001} # hyperparameters
 
 # ---- options ----
-DEVICE_ID = 'cpu' # set to 'cpu' for cpu, 'cuda' / 'cuda:0' or similar for gpu.
+DEVICES = {False: 'cpu', True: 'cuda'}
+# DEVICE_ID = 'cpu' # set to 'cpu' for cpu, 'cuda' / 'cuda:0' or similar for gpu.
 LOG_DIR = 'checkpoints'
 VISUALIZE = False # set True to visualize input, prediction and the output from the last batch
-LOAD_CHKPT = False
+LOAD_CHECKPOINT = False
 
 # --- imports ---
-import torch
 import os
-import matplotlib.pyplot as plt
+
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torchvision.transforms as transforms
-import the3utils
-torch.multiprocessing.set_start_method('spawn', force=True)
+
+from the3utils import *
+
 # ---- utility functions -----
 def get_loaders(batch_size,device):
     data_root = 'ceng483-s19-hw3-dataset' 
-    train_set = hw3utils.HW3ImageFolder(root=os.path.join(data_root,'train'),device=device)
+    train_set = HW3ImageFolder(root=os.path.join(data_root,'train'),device=device)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0)
-    val_set = hw3utils.HW3ImageFolder(root=os.path.join(data_root,'val'),device=device)
+    val_set = HW3ImageFolder(root=os.path.join(data_root,'val'),device=device)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=0)
     # Note: you may later add test_loader to here.
     return train_loader, val_loader
 
-# ---- ConvNet -----
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 3, 5, padding=2)
+def get_pad_count(n, s, f):
+    return int(((n*s)-s-n+f) / 2)
 
-    def forward(self, grayscale_image):
-        # apply your network's layers in the following lines:      
-        x = self.conv1(grayscale_image)
+# ---- ConvNet -----
+# TODO: Construct network
+class ColorNet(nn.Module):
+    def __init__(self):
+        super(ColorNet, self).__init__()
+        # Adding layers
+        self.layers = []
+        # Get padding needed
+        pad = get_pad_count(80, 1, ARGS.kernelsize)
+        print(pad)
+        # Starting conv Layer: (input image channel: 1, output channel: 1*K, square kernel: FxF)
+        clayer = nn.Conv2d(in_channels=1, out_channels=ARGS.kernelcount, kernel_size=ARGS.kernelsize, 
+                           bias=True, padding=pad)
+        relu = nn.ReLU()
+        self.layers.extend([clayer, relu])
+        for i in range(1, ARGS.clayers-1):
+            # Conv Layer: (input image channel: K, output channel: K*K, square kernel: FxF)
+            clayer = nn.Conv2d(in_channels=ARGS.kernelcount, out_channels=ARGS.kernelcount, kernel_size=ARGS.kernelsize, 
+                               bias=True, padding=pad)
+            # Apply ReLU
+            relu = nn.ReLU()
+            self.layers.extend([clayer, relu])
+        clayer = nn.Conv2d(in_channels=ARGS.kernelcount, out_channels=3, kernel_size=ARGS.kernelsize, 
+                           bias=True, padding=pad)
+        self.layers.append(clayer)
+        # Place layers in a sequence 
+        self.layers = nn.Sequential(*self.layers)
+        print(self.layers)
+
+    def forward(self, grayscale_image):   
+        x = self.layers(grayscale_image)
         return x
 
-# ---- training code -----
-device = torch.device(DEVICE_ID)
-print('device: ' + str(device))
-net = Net().to(device=device)
-criterion = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr=hps['lr'])
-train_loader, val_loader = get_loaders(batch_size,device)
+def train():
+    # ---- training code -----
+    device = torch.device(DEVICES[ARGS.gpu])
+    print('Device: ' + str(device))
+    net = ColorNet().to(device=device)
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=ARGS.learnrate) # learning rate
+    train_loader, val_loader = get_loaders(ARGS.batchsize, device)
 
-if LOAD_CHKPT:
-    print('loading the model from the checkpoint')
-    model.load_state_dict(os.path.join(LOG_DIR,'checkpoint.pt'))
+    if LOAD_CHECKPOINT:
+        print('Loading the model from the checkpoint')
+        model.load_state_dict(os.path.join(LOG_DIR, 'checkpoint.pt'))
 
-print('training begins')
-for epoch in range(max_num_epoch):  
-    running_loss = 0.0 # training loss of the network
-    for iteri, data in enumerate(train_loader, 0):
-        inputs, targets = data # inputs: low-resolution images, targets: high-resolution images.
+    print('Training begins:')
+    for epoch in range(100):#ARGS.maxepoch):  
+        running_loss = 0.0 # training loss of the network
+        for iteri, data in enumerate(train_loader, 0):
+            inputs, targets = data # inputs: low-resolution images, targets: high-resolution images.
 
-        optimizer.zero_grad() # zero the parameter gradients
+            optimizer.zero_grad() # zero the parameter gradients
 
-        # do forward, backward, SGD step
-        preds = net(inputs)
-        loss = criterion(preds, targets)
-        loss.backward()
-        optimizer.step()
+            # do forward, backward, SGD step
+            preds = net(inputs)
+            loss = criterion(preds, targets)
+            loss.backward()
+            optimizer.step()
 
-        # print loss
-        running_loss += loss.item()
-        print_n = 100 # feel free to change this constant
-        if iteri % print_n == (print_n-1):    # print every print_n mini-batches
-            print('[%d, %5d] network-loss: %.3f' %
-                  (epoch + 1, iteri + 1, running_loss / 100))
-            running_loss = 0.0
-            # note: you most probably want to track the progress on the validation set as well (needs to be implemented)
+            # print loss
+            running_loss += loss.item()
+            print_n = 100 # feel free to change this constant
+            if iteri % print_n == (print_n-1):    # print every print_n mini-batches
+                print('[%d, %5d] network-loss: %.3f' %
+                    (epoch + 1, iteri + 1, running_loss / 100))
+                running_loss = 0.0
+                # note: you most probably want to track the progress on the validation set as well (needs to be implemented)
 
-        if (iteri==0) and VISUALIZE: 
-            hw3utils.visualize_batch(inputs,preds,targets)
+            if (iteri==0) and VISUALIZE: 
+                visualize_batch(inputs,preds,targets)
 
-    print('Saving the model, end of epoch %d' % (epoch+1))
-    if not os.path.exists(LOG_DIR):
-        os.makedirs(LOG_DIR)
-    torch.save(net.state_dict(), os.path.join(LOG_DIR,'checkpoint.pt'))
-    hw3utils.visualize_batch(inputs,preds,targets,os.path.join(LOG_DIR,'example.png'))
+        print('Saving the model, end of epoch %d' % (epoch+1))
+        if not os.path.exists(LOG_DIR):
+            os.makedirs(LOG_DIR)
+        torch.save(net.state_dict(), os.path.join(LOG_DIR, 'checkpoint.pt'))
+        visualize_batch(inputs,preds,targets,os.path.join(LOG_DIR, 'example.png'))
 
-print('Finished Training')
+    print('Finished training!')
 
+def test():
+    pass
 
+def get_current_config():
+    global ARGS
+    """Return a string indicating current parameter configuration"""
+    config = vars(ARGS)
+    message = "\nRunning with the following parameter settings:\n"
+    separator = "-" * (len(message)-2) + "\n"
+    lines = ""
+    for item, key in config.items():
+        lines += "- {}: {}\n".format(item, key)
+    return (message + separator + lines + separator) 
+
+def show_current_config():
+    """Print current parameter configuration"""
+    print(get_current_config())
+
+def main():
+    global ARGS
+    torch.multiprocessing.set_start_method('spawn', force=True)
+    ARGS = arg_handler()
+    # If required args are parsed properly
+    if ARGS:
+        show_current_config()
+        train()
+
+if __name__ == "__main__":
+    main()
--- the3utils.py
+++ the3utils.py
@@ -1,8 +1,11 @@
 # These are utility functions / classes that you probably dont need to alter.
 
+import argparse
+import sys
+
+import matplotlib.pyplot as plt
 import torchvision
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 
 def tensorshow(tensor,cmap=None):
     img = transforms.functional.to_pil_image(tensor/2+0.5)
@@ -10,6 +13,96 @@ def tensorshow(tensor,cmap=None):
         plt.imshow(img,cmap=cmap)
     else:
         plt.imshow(img)
+
+# Disable multiple occurences of the same flag
+class UniqueStore(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        if getattr(namespace, self.dest, self.default) is not None:
+            parser.error(option_string + " appears several times.")
+        setattr(namespace, self.dest, values)
+
+# Custom format for arg Help print
+class CustomFormatter(argparse.HelpFormatter):
+    def __init__(self,
+                 prog,
+                 indent_increment=2,
+                 max_help_position=100, # Modified
+                 width=200):
+        super().__init__(prog, indent_increment, max_help_position, width)
+
+    def _format_action_invocation(self, action):
+        if not action.option_strings:
+            metavar, = self._metavar_formatter(action, action.dest)(1)
+            return metavar
+        else:
+            parts = []
+            if action.nargs == 0:
+                parts.extend(action.option_strings)
+            else:
+                default = action.dest.upper()
+                args_string = self._format_args(action, default)
+                for option_string in action.option_strings:
+                    parts.append('%s' % option_string)
+                parts[-1] += ' %s' % args_string
+            return ', '.join(parts)
+
+def check_positive(value):
+    try:
+        value = int(value)
+        assert (value > 0)
+    except Exception as e:
+        raise argparse.ArgumentTypeError("Positive integer is expected but got: {}".format(value))
+    return value
+
+# def check_epoch(value):
+#     try:
+#         value = int(value)
+#         assert (value > 0) and (value < 100)
+#     except Exception as e:
+#         raise argparse.ArgumentTypeError("Positive integer less than 100 is expected but got: {}".format(value))
+#     return value  
+
+def check_lr(value):
+    try:
+        value = float(value)
+        assert (value > 0.0001) and (value < 0.1)
+    except Exception as e:
+        raise argparse.ArgumentTypeError("Float between 0.0001 & 0.1 is expected but got: {}".format(value))
+    return value
+
+# Handles cmd args
+def arg_handler():
+    parser = argparse.ArgumentParser(description='Image Colorization with Torch', 
+                                     formatter_class=CustomFormatter, 
+                                     add_help=False)
+    # Optional flags
+    parser.add_argument("-h", "--help", help="Help message", action="store_true")
+    parser.add_argument("--gpu", help="Use GPU", action="store_true", default=False)
+
+    # Required flags
+    enable_exec = ("-h" not in sys.argv)
+    group = parser.add_argument_group(title='required arguments')
+    group.add_argument("-bs", "--batchsize",  help="Specify batch size", type=check_positive, 
+                       metavar="BATCH", required=enable_exec)
+    # group.add_argument("-me", "--maxepoch",  help="Specify max number of epochs", type=check_epoch, 
+    #                    metavar="EPOCH", required=enable_exec)
+    group.add_argument("-cl", "--clayers",  help="Specify number of convolutional layers (e.g. 1, 2, 4)", 
+                       type=check_positive, metavar="CONV", required=enable_exec)
+    group.add_argument("-ks", "--kernelsize",  help="Specify kernel size (e.g. 3, 5)", type=check_positive, 
+                       metavar="SHAPE", required=enable_exec)
+    group.add_argument("-kc", "--kernelcount",  help="Specify number of kernels (e.g. 2, 4, 8)", type=check_positive,
+                       metavar="COUNT", required=enable_exec)
+    group.add_argument("-lr", "--learnrate",  help="Specify learning rate (e.g. in range (0.0001, 0.1))", 
+                       type=check_lr, metavar="RATE", required=enable_exec)
+
+    args = parser.parse_args()
+
+    # Print help if -h is used
+    if args.help:
+        parser.print_help()
+        return
+
+    return args
 
 class HW3ImageFolder(torchvision.datasets.ImageFolder):
     """A version of the ImageFolder dataset class, customized for the super-resolution task"""
